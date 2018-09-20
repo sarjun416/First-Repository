0b84232f1287b6b2b5e6d2c2fc3fe2e46f7bc7c0
adminuser <adminuser@localhost.localdomain>
2018-09-20 12:56:34 -0400

    hello file

---
+++ hello.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# this is a comment.
+echo "Hello everyone" # this is also a comment
+
+echo our shell name is $BASH
+echo our shell version is $BASH_VERSION
+echo our home directory is $HOME
+echo current working directory is $PWD
+
+name=Mark
+val=10
+echo the name is $name
+echo value $val
+
